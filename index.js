0acbf46ddb1fe692c5453045880005ef9e330951
active-tutorial-1080 <gareth.shackleton@wework.com>
2018-09-19 19:35:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let currentUser = "Gareth"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "Gareth" + "!"
+
 
 
 
