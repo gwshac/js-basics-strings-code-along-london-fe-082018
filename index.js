b43ac01d1ff0a2ba442f4d4ff245142a4780f4fe
active-tutorial-1080 <gareth.shackleton@wework.com>
2018-09-19 19:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let currentUser = "Gareth"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
-let shortGreeting = "Welcome, " + firstInitial
 const firstInitial = currentUser(G);
 const restOfName = currentUser.slice(1)
+let shortGreeting = "Welcome, " + firstInitial
+
+
 
 
 
